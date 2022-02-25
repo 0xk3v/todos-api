fd4843e703b4e00b1bda7b0096aed78ed6d4afd8
n-kev1n <kevin_@tuta.io>
2022-02-25 14:25:49 +0100

    server index file

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("Hola");
+"use strict";
+
+const express = require("express");
+const dotenv = require("dotenv");
+const port = 5000 || process.env.PORT;
+
+const app = express();
+
+app.get("/api/todos", (req, res) => {
+  res.send({ message: "hola" });
+});
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
