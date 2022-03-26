311be142da9578d0ec2cbc4df037933450765642
n-kev1n <kevin_@tuta.io>
2022-03-26 13:11:02 +0100

    todos controllers and routes setup

--- src/controllers/todoControllers.js
+++ src/controllers/todoControllers.js
@@ -1,34 +1,40 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc GET todos
 // @route /api/todos
 // @access Private
 
-const getTodos = (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Todos" });
-};
+});
 
 // @desc POST todos
 // @route /api/todos
 // @access Private
 
-const setTodos = (req, res) => {
+const setTodos = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({ message: "Set Todos" });
-};
+});
 
 // @desc PUT todos
 // @route /api/todos/:id
 // @access Private
 
-const updateTodos = (req, res) => {
+const updateTodos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Todo ${req.params.id}` });
-};
+});
 
 // @desc DELETE todos
 // @route /api/todos/:id
 // @access Private
 
-const deleteTodos = (req, res) => {
+const deleteTodos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Todo ${req.params.id}` });
-};
+});
 
 module.exports = {
   getTodos,
--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@
 
 const express = require("express");
 const dotenv = require("dotenv");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = 5000 || process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/todos", require("./routes/todoRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
